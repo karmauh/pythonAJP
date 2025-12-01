0273bda68fcccaaab677671281bb3e3f7f3f97b0
karmauh <mikolaj.bozko01@gmail.com>
2025-12-01 20:32:01 +0100

    final

--- board.py
+++ board.py
@@ -3,12 +3,15 @@ class Board:
         self.n = n
         self.queens = []
 
+    # dodanie krolowej na planszy
     def place(self, row, col):
         self.queens.append((row, col))
 
+    # usuniecie krolowej z planszy
     def remove(self, row, col):
         self.queens.remove((row, col))
 
+    # sprawdzenie ustawionych juz krolowych
     def is_safe(self, row, col):
         for qr, qc in self.queens:
             if qr == row or qc == col:
@@ -17,14 +20,16 @@ class Board:
                 return False
         return True
 
+    # lista rozwiazan
     def _solve_recursive(self, col=0, solutions=None):
         if solutions is None:
             solutions = []
-
+        # jesli ustawiono krolowa w kazdej kolumnie to mamy rozwiazanie
         if col == self.n:
             solutions.append(list(self.queens))
             return solutions
 
+        # ustawiamy krolowa w kazdym wierszu danej kolumny
         for row in range(self.n):
             if self.is_safe(row, col):
                 self.place(row, col)
@@ -36,6 +41,7 @@ class Board:
     def solve(self):
         return self._solve_recursive()
 
+    # tekstowa reprezantacjsa planszy
     def __str__(self):
         rows = []
         for r in range(self.n):
@@ -56,6 +62,7 @@ def solve(n=8):
     board = Board(n)
     return board.solve()
 
+# testy z roznymi n
 if __name__ == "__main__":
     for n in [1, 2, 3, 4, 8]:
         solutions = solve(n)
